55635df6ee5e3156a83f1754a23d057821b8bd25
kopp4 <kopp4@users.noreply.github.com>
2023-03-15 18:26:10 +0800

    🧹 and notification with sound in a not elegant way

--- scripts/default/enum_theme.sh
+++ scripts/default/enum_theme.sh
@@ -3,13 +3,16 @@
 # if koppa conf is commented
 if grep -q "^#.*apatheia/theme.conf.koppa" ~/.config/hypr/hyprland.conf; then
     # uncomment koppa
-    echo 'uncomment koppa'
     sed -i '/theme\.conf\.koppa/s/^#//' ~/.config/hypr/hyprland.conf
     sed -i '/\/apatheia\/theme\.conf$/s/^/#/' ~/.config/hypr/hyprland.conf
+    # dunstify -u normal 'changed to work mode !' 'お仕事頑張ってね'
+    dunstify -u normal 'お仕事頑張ってね'
+
 
 else
     # comment koppa
-    echo 'comment koppa'
     sed -i '/theme\.conf\.koppa/s/^/#/' ~/.config/hypr/hyprland.conf
     sed -i '/\/apatheia\/theme\.conf$/s/^#//' ~/.config/hypr/hyprland.conf
+    # dunstify -u normal 'changed to normal mode !' 'お疲れ様でした'
+    dunstify -u normal 'お疲れ様でした'
 fi
