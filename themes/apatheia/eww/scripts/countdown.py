ef0f7864f8d3de78a77ef6f4688ce93d10ee2825
kopp4 <kopp4@users.noreply.github.com>
2024-10-04 22:34:53 +0800

    todoist ddl update

--- themes/apatheia/eww/scripts/countdown.py
+++ themes/apatheia/eww/scripts/countdown.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
-import sys
+
 import datetime
+import sys
+import calendar
+
+
+def print_help():
+    help_text = """
+Usage: python script.py [options]
+
+Options:
+  print(" option: ./x.py DD/MM/YYYY event")
+  -h, --help  Show this help message and exit.
+"""
+    print(help_text)
+
 
 def date_subtract(ddldate):
     """
@@ -41,17 +55,42 @@ def format_output(ddl, event):
 
 
 
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        if (sys.argv[1].lower() == '-h' or sys.argv[1].lower() == '--help'):
+            print_help()
+            sys.exit()
+        try:
+            ddl = get_date(sys.argv[1])
+            event = get_event(sys.argv[2:])
+            format_output(ddl, event)
+        except Exception as e:
+            print("DDL is not working it wont fix itself")
 
-if len(sys.argv) > 1:
 
-    try:
-        ddl = get_date(sys.argv[1])
-        event = get_event(sys.argv[2:])
-        format_output(ddl, event)
-    except Exception as e:
-        print("DDL is not working it wont fix itself")
-else:
+    else:
+        """
+        Print without parameters
 
-    print(" option: ./x.py DD/MM/YYYY event")
+        1. days left current month
+        2. current month
+        """
+
+        today = datetime.date.today()
+        current_month = today.month
+        current_year = today.year
+        # ddl = get_date(sys.argv[1])
+        # event = get_event(sys.argv[2:])
+        # format_output(ddl, event)
+
+
+        _, number_of_days = calendar.monthrange(current_year, current_month)
+    
+        # Calculate remaining days
+        remaining_days = number_of_days - today.day
+    
+        # print(f"Remaining days in {calendar.month_name[current_month]} {current_year}: {remaining_days}")
+        print(f"{remaining_days}")
+        print(f"{calendar.month_name[current_month]}")
 
 
